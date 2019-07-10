d589478a19f4df7061116c0505995607ffd1b586
Jack Atherton <jackrrye@gmail.com>
2019-07-10 22:10:44 +0000

    Done.

---
+++ lib/Artist.rb
@@ -0,0 +1,38 @@
+require 'pry'
+
+class Artist
+
+  attr_accessor :name, :songs
+
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @songs = []
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.destroy_all
+    @@all.clear
+  end
+
+  def save
+    @@all << self
+  end
+
+  def self.create(name)
+    artist = Artist.new(name)
+    artist.save
+    artist
+  end
+
+  #spec 004
+  def add_song(song)
+    song.artist == nil ? song.artist = self : song.artist
+    @songs.include?(song) ? song : @songs << song
+  end
+
+end
---
+++ lib/Song.rb
@@ -0,0 +1,39 @@
+require 'pry'
+
+class Song
+
+  attr_accessor :name, :artist
+
+  @@all = []
+
+  def initialize(name, artist = nil)
+    @name = name
+    self.artist= (artist)
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.destroy_all
+    @@all.clear
+  end
+
+  def save
+    @@all << self
+  end
+
+  def self.create(name)
+    song = Song.new(name)
+    song.save
+    song
+  end
+
+  #spec 004
+  def artist= (artist)
+    @artist = artist
+    artist.add_song(self)
+  end
+
+
+end
