b95d7bfa86116e87c562a14c6203e31db4ee35db
Maasa <konomaasa@gmail.com>
2019-07-28 20:10:44 -0700

    Done.

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -8,7 +8,8 @@ class MusicLibraryController
     end
 
     def call
-        input = gets.chomp until input == "exit"
+        input = ""
+        until input == "exit"
             puts "Welcome to your music library!"
             puts "To list all of your songs, enter 'list songs'."
             puts "To list all of the artists in your library, enter 'list artists'."
@@ -18,6 +19,24 @@ class MusicLibraryController
             puts "To play a song, enter 'play song'."
             puts "To quit, type 'exit'."
             puts "What would you like to do?"
+
+        input = gets.chomp
+
+        case input 
+            when "list songs"
+                list_songs
+            when "list artists"
+                list_artists
+            when "list genres"
+                list_genres
+            when "list artist"
+                list_songs_by_artist
+            when "list genre"
+                list_songs_by_genre
+            when "play song"
+                play_song
+            end
+        end
     end 
 
     def list_songs
@@ -40,6 +59,28 @@ class MusicLibraryController
         if artist
             artist.songs.sort {|a, b| a.name <=> b.name}.each_with_index {|song, index| puts "#{index +1}. #{song.name} - #{song.genre.name}"}
         end
+    end
+
+    def list_songs_by_genre
+        puts "Please enter the name of a genre:"
+        input = gets.chomp
+        genre = Genre.find_by_name(input)
+
+        if genre
+            genre.songs.sort {|a, b| a.name <=> b.name}.each_with_index {|song, index| puts "#{index +1}. #{song.artist.name} - #{song.name}"}
+        end
+    end
+
+    def play_song
+        puts "Which song number would you like to play?"
+        input = gets.chomp
+        input = input.to_i
+
+        if (1..Song.all.size).include?(input)
+            sorted_songs = Song.all.sort {|a, b| a.name <=> b.name}
+            song = sorted_songs[input - 1]
+        end
+        puts "Playing #{song.name} by #{song.artist.name}" if song
     end
 
 end
