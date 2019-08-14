059a73087a6ff1c595a765a925ed97ef19705eb0
Adjoa Ntoso <adjoaan@gmail.com>
2019-08-14 01:05:00 +0000

    Done.

--- config/environment.rb
+++ config/environment.rb
@@ -2,6 +2,19 @@ require 'bundler'
 Bundler.require
 
 module Concerns
+  module Findable
+     def self.find_by_name(name)
+    self.all.find {|song| song.name == name}
+  end
+
+   def self.find_or_create_by_name(name)
+     if !self.find_by_name(name)
+        Song.create(name)
+      else
+        self.find_by_name(name)
+    end
+  end
+  end
 end
 
-require_all 'lib'
+require_all 'lib' 
